17c065ec78502cf0599dfb44d621232676100d08
zhuangqf <zhuangqinfa@outlook.com>
2016-10-30 22:48:15 +0800

    change to react 1.0

---
+++ js_react/footer.js
@@ -0,0 +1,7 @@
+/**
+ * Created by zhuangqf on 10/30/16.
+ */
+ReactDOM.render(
+    <h3>zhuangqf<span className="glyphicon glyphicon-heart"></span>chuyj</h3>,
+    document.getElementById('footer')
+);
